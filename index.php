ea5023f920c7ecf232bace7eddbc87b06f3fa5c5
Conormacpherson <Cmacpherson1766@conestogac.on.ca>
2018-12-06 10:13:40 -0500

    Add file reading php page Add test jquery & ajax

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Text Editor</title>
+    </head>
+    <body>
+    
+    <h1>Hello</h1>
+
+    <?php
+        $files = scandir("/MyFiles");
+        echo $files;
+    ?>
+
+    </body>
+
+</html>
